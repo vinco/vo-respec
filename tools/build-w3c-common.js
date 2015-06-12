c49c787d1bd4fdab83e43cb233c82fda0aa54fbb
Alexander Salas <a.salas@ieee.org>
2015-06-12 07:02:47 -0430

    New output of the build to faster test

--- tools/build-w3c-common.js
+++ tools/build-w3c-common.js
@@ -4,7 +4,7 @@ var fs   = require("fs")
 ,   pth  = require("path")
 ,   b    = require("./builder")
 ,   version = JSON.parse(fs.readFileSync(pth.join(__dirname, "../package.json"), "utf-8")).version
-,   builds = pth.join(__dirname, "../builds")
+,   builds = pth.join(__dirname, "../assets/js")
 ,   latest = pth.join(builds, "respec-w3c-common.js")
 ;
 
